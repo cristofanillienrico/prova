9151b5489827bdfb57e6023853ca5480e4647eb9
Solving-Enrico <cristofanillienrico@gmail.com>
2021-06-08 10:57:58 +0200

    2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    NavbarComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
